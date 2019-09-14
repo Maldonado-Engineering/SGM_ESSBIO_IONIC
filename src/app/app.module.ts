59c3543de60a4e81f498b2ae06c9704efef3250d
mauricio <mauricio.maldonado@uniacc.edu>
2019-09-14 14:38:16 -0300

    primera subida de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { ComponentsModule } from './components/components.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AppVersion,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
